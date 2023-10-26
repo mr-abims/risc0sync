2ce23699d56c57a01b1283f9bd247c41696aa03e
mr-abims <adebayoabimbola9@gmail.com>
2023-10-26 06:24:58 +0100

    test

--- host/src/main.rs
+++ host/src/main.rs
@@ -43,3 +43,47 @@ fn main() {
 
     println!("The {}th block has hashPrevBlock = {}", NUM_BLOCKS, digest);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::{Once, ONCE_INIT};
+
+    static START: Once = ONCE_INIT;
+
+    fn setup() {
+        START.call_once(|| {
+            // Any setup code you might need, like mocking endpoints, should be placed here
+        });
+    }
+
+    // Mock for reqwest
+    fn mock_get(url: &str) -> Result<String, reqwest::Error> {
+        // Here we return mock data based on the input URL
+        match url {
+            "https://blockstream.info/api/block-height/0" => Ok("mock_block_id_0".to_string()),
+            "https://blockstream.info/api/block/0/header" => Ok("mock_header_0".to_string()),
+            // ... continue this pattern for all blocks up to `NUM_BLOCKS`
+            _ => Err(reqwest::Error::new(reqwest::StatusCode::NOT_FOUND, "not found")),
+        }
+    }
+
+    #[test]
+    fn test_fetch_and_verify_block_headers() {
+        setup();
+        
+        // Capture the output of your main function using a hook
+        let output = std::io::Cursor::new(Vec::new());
+        let old_stdout = std::io::stdout();
+        std::io::set_stdout(Box::new(output));
+
+        main(); // Execute the main function
+        
+        // Reset stdout and get the captured output
+        std::io::set_stdout(old_stdout);
+        let output = String::from_utf8(output.into_inner()).unwrap();
+
+        // Check that the expected block headers were processed and the output matches our expectations
+        assert!(output.contains(&format!("Received {} block headers from blockstream.info", NUM_BLOCKS)));
+        assert!(output.contains(&format!("The {}th block has hashPrevBlock =", NUM_BLOCKS)));
+    }
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -109,3 +109,89 @@ pub fn main() {
         else { hash_prev_block.copy_from_slice(&hash256.as_bytes()); }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use reqwest;
+    use zkvm_env::env;
+
+    const BASE_URL: &str = "https://blockstream.info/api";
+
+    fn fetch_block_height() -> Result<u32, reqwest::Error> {
+        let response: u32 = reqwest::blocking::get(&format!("{}/block-height/", BASE_URL))?.json()?;
+        Ok(response)
+    }
+
+    fn fetch_block_header(block_height: u32) -> Result<[u8; 80], reqwest::Error> {
+        let response: Vec<u8> = reqwest::blocking::get(&format!("{}/block/{}/header", BASE_URL, block_height))?.bytes()?.to_vec();
+        let mut header = [0u8; 80];
+        header.copy_from_slice(&response);
+        Ok(header)
+    }
+
+    #[test]
+    fn test_main_valid_block() {
+        let block_height = fetch_block_height().expect("Failed to fetch block height");
+
+        let block_header = fetch_block_header(block_height).expect("Failed to fetch block header");
+
+        env::set_test_data(block_height, block_header);
+
+        // Run the main function
+        main();
+
+       
+        let committed_data = env::get_committed_data();
+        assert!(committed_data.is_some());
+        let committed_data_ref = committed_data.as_ref().unwrap();
+
+        // Compute expected commit value based on fetched header
+        let sha256 = Impl::hash_bytes(&block_header);
+        let expected_commit_data = Impl::hash_bytes(&sha256.as_bytes());
+
+        assert_eq!(committed_data_ref, &expected_commit_data.as_bytes());
+    }
+}
+
+mod zkvm_env {
+    #[cfg(not(test))]
+    pub use risc0_zkvm::guest::env::*;
+
+    #[cfg(test)]
+    pub mod env {
+        static mut TEST_BLOCK_HEIGHT: Option<u32> = None;
+        static mut TEST_HEADER: Option<[u8; 80]> = None;
+        static mut COMMITTED_DATA: Option<Vec<u8>> = None;
+
+        pub fn read() -> u32 {
+            unsafe {
+                TEST_BLOCK_HEIGHT.expect("Block height not set for test")
+            }
+        }
+
+        pub fn read_slice(buffer: &mut [u8]) {
+            unsafe {
+                buffer.copy_from_slice(&TEST_HEADER.expect("Header not set for test"));
+            }
+        }
+
+        pub fn commit(data: &[u8]) {
+            unsafe {
+                COMMITTED_DATA = Some(data.to_vec());
+            }
+        }
+
+        pub fn get_committed_data() -> Option<Vec<u8>> {
+            unsafe { COMMITTED_DATA.clone() }
+        }
+
+        pub fn set_test_data(block_height: u32, header: [u8; 80]) {
+            unsafe {
+                TEST_BLOCK_HEIGHT = Some(block_height);
+                TEST_HEADER = Some(header);
+            }
+        }
+    }
+}
